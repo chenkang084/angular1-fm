d29a6a29d3fe0e3e6e89018de564ad7750aeb4b8
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-02-07 16:34:01 +0800

    resolve conflict

--- app/config/config.local.js
+++ app/config/config.local.js
@@ -3,16 +3,12 @@ const config = {
     uri: {
         base: '/',
         host: '/',
-<<<<<<< HEAD
         api: "https://customerdna.corp.ebay.com/game-be/"
     },
     user: {
         nt: 'kanchen',
         firstName: 'Kang',
         lastName: 'Chen'
-=======
-        api: "http://localhost:8080/game-be/"
->>>>>>> master
     }
 }
 
